9406b0c60157872faab6b34f2f080665cf6ba231
James Lentzsch <jrlentzsch@gmail.com>
2021-06-06 14:26:43 -0700

    further work on enigma frontend components

---
+++ react-app/src/components/enigma/Plugboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Plugboard = () => {
+    return (
+        <div className="plugboard">
+            <h1>This will be the plugboard</h1>
+        </div>
+    )
+}
+
+export default Plugboard;
