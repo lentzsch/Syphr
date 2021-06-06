c7e8de92710b65c808b96d5ffda026ff06cfbf65
James Lentzsch <jrlentzsch@gmail.com>
2021-06-05 20:33:42 -0700

    more enigma testing cleaned up blugboard issue

--- app/enigma/enigma.py
+++ app/enigma/enigma.py
@@ -14,15 +14,23 @@ from plugboard import Plugboard
 # print(new_plugboard)
 # print(new_plugboard.plugboard_encrypt('D'))
 
-# plugboard = Plugboard([(0, 2), (3, 5), (6, 1)])
+plugboard = Plugboard([(0, 3), (4, 5), (6, 7)])
 rotor_one = Rotor('III', 0)
 rotor_two = Rotor('II', 0)
 rotor_three = Rotor('I', 0)
 reflector = Reflector('B')
-def encrypt():
-    for _ in range(5):
-        # char = plugboard.plugboard_swap(0)
-        char = 0
+def encrypt(message):
+    # set the reference alphabet and new message.
+    alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    new_message = ''
+
+    for char in message:
+        # get the index for the character in the alphabet
+        char = alpha.index(char)
+        # begin encryption
+        # first pass through plugboard
+        char = plugboard.plugboard_swap(char)
+        #first pass through rotors and rotation of rotors
         rotor_one.rotate()
         char = rotor_one.rotor_in(char)
         if rotor_one.alpha[0] in rotor_one.notch:
@@ -31,15 +39,18 @@ def encrypt():
         if rotor_two.alpha[0] in rotor_two.notch:
             rotor_three.rotate()
         char = rotor_three.rotor_in(char)
+        # reflector bounces "signal" back through machine
         char = reflector.reflector_return(char)
+        # second run back through rotors with no rotation
         char = rotor_three.rotor_out(char)
         char = rotor_two.rotor_out(char)
         char = rotor_one.rotor_out(char)
-        def final_form(index):
-            alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-            return alpha[index]
-            
-        final_char = final_form(char)
-        print('FINAL CHAR ---------->', final_char)
+        # second run through the plugboard and out the machine
+        char = plugboard.plugboard_swap(char)
 
-encrypt()
+        new_message += alpha[char]
+
+    return new_message
+
+
+print(encrypt('XKBADDNHL'))
--- app/enigma/plugboard.py
+++ app/enigma/plugboard.py
@@ -5,13 +5,12 @@ class Plugboard:
         self.base = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
         self.config = {}
 
-        for char in self.base:
+        for char in range(len(self.base)):
             self.config[char] = char
 
         for chars in config:
             self.config[chars[0]] = chars[1]
             self.config[chars[1]] = chars[0]
-
     
     def plugboard_swap(self, char):
         return self.config[char]
