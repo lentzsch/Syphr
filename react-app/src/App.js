e861254b565b94b1f6b6d211bdd0d18e4aed6af6
James Lentzsch <jrlentzsch@gmail.com>
2021-06-06 20:09:33 -0700

    more frontend components and layout

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,6 +5,7 @@ import LoginForm from "./components/auth/LoginForm";
 import SignUpForm from "./components/auth/SignUpForm";
 import NavBar from "./components/NavBar";
 import Enigma from "./components/Enigma"
+import MessagesBox from "./components/MessagesBox"
 import ProtectedRoute from "./components/auth/ProtectedRoute";
 import UsersList from "./components/UsersList";
 import User from "./components/User";
@@ -29,6 +30,7 @@ function App() {
   return (
     <BrowserRouter>
       <NavBar />
+      <MessagesBox />
       <Enigma />
       {/* <Switch>
         <Route path="/login" exact={true}>
