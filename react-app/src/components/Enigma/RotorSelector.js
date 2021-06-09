9ead5ef6f9cca617b713d5d952889761f62e18bf
James Lentzsch <jrlentzsch@gmail.com>
2021-06-09 10:52:53 -0700

    rotor selector display issue solved

--- react-app/src/components/Enigma/RotorSelector.js
+++ react-app/src/components/Enigma/RotorSelector.js
@@ -4,20 +4,24 @@ import './RotorSelector.css'
 const RotorSelector = () => {
     return (
         <div className="rotor-selector">
-            <div className="rotor-one">
-                <h1>I</h1>
+            <div className="rotor-selector-top-row">
+                <div className="rotor-one">
+                    <h1>I</h1>
+                </div>
+                <div className="rotor-two">
+                    <h1>II</h1>
+                </div>
+                <div className="rotor-three">
+                    <h1>III</h1>
+                </div>
             </div>
-            <div className="rotor-two">
-                <h1>II</h1>
-            </div>
-            <div className="rotor-three">
-                <h1>III</h1>
-            </div>
-            <div className="rotor-four">
-                <h1>IV</h1>
-            </div>
-            <div className="rotor-five">
-                <h1>V</h1>
+            <div className="rotor-selector-bottom-row">
+                <div className="rotor-four">
+                    <h1>IV</h1>
+                </div>
+                <div className="rotor-five">
+                    <h1>V</h1>
+                </div>
             </div>
         </div>
     )
--- react-app/src/components/Enigma/Rotors.js
+++ react-app/src/components/Enigma/Rotors.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './Rotors.css';
 
 const Rotors = () => {
 
