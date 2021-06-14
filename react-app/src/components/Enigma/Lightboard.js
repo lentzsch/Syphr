1a83f7a82fe73433a000d1b62e22396715528285
James Lentzsch <jrlentzsch@gmail.com>
2021-06-13 20:55:49 -0700

    linked lightboard and output

--- react-app/src/components/Enigma/Lightboard.js
+++ react-app/src/components/Enigma/Lightboard.js
@@ -1,20 +1,31 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import './Lightboard.css'
 
 const Lightboard = () => {
-
+    const currentChar = useSelector(state => state.enigma.currentChar)
     const lightboardAlpha = 'QWERTZUIOASDFGHJKPYXCVBNML'.split('')
 
     return (
         <div className="lightboard">
             {lightboardAlpha.map((char) => {
-                return (
-                    <div className="character-container lightboard"
-                         style={{ gridPosition: char }}
-                    >
-                        {char}
-                    </div>
-                )
+                if (char === currentChar) {
+                    return (
+                        <div className="character-container-currentChar lightboard"
+                            style={{ gridPosition: char }}
+                        >
+                            {char}
+                        </div>
+                    )
+                } else {
+                    return (
+                        <div className="character-container lightboard"
+                            style={{ gridPosition: char }}
+                        >
+                            {char}
+                        </div>
+                    )
+                }
             })}
         </div>
     )
--- react-app/src/components/MessagesBox.js
+++ react-app/src/components/MessagesBox.js
@@ -5,13 +5,6 @@ import './MessagesBox.css'
 const MessagesBox = () => {
     const conversation = useSelector(state => state.conversation);
     const currentConversation = conversation.current;
-    console.log("CURRENT CONVERSATION ------->", currentConversation);
-    // console.log("CURRENT CONVO MESSAGES ------->", currentConversation.messages)
-    // if (currentConversation === null) {
-    //     return null
-    // }
-    // const messages = currentConversation.messages
-    // console.log("MESSAGES --------->", messages[0])
     return (
         <div className="messages-box-container">
             {currentConversation?.messages.map((message) => (
