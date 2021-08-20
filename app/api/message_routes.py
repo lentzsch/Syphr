16ae118e322489e8144f7a7b464f76861a72da48
James Lentzsch <jrlentzsch@gmail.com>
2021-08-20 10:22:31 -0700

    create new conversation fixed clean up still needed

--- app/api/message_routes.py
+++ app/api/message_routes.py
@@ -1,7 +1,7 @@
 from app.forms.message_encryption_form import MessageEncryptionForm
 from flask import Blueprint, session, request
 from flask_login import current_user, login_required
-from app.models import Message, Conversation, db
+from app.models import Message, Conversation, User, db
 from app.enigma import encrypt
 
 
@@ -18,13 +18,22 @@ def get_conversation(partnerId):
 
 
 ####################### CREATE NEW CONVERSATION ##############################
-@message_routes.route('/<int:partnerId>/new')
-@login_required
-def create_new_conversation(partnerId):
-        new_conversation = Conversation(
+@message_routes.route('/conversation/new', methods=['POST'])
+# @login_required
+def create_new_conversation():
+        data=request.json
+        userId=data["userId"]
+        partnerId=data["partnerId"]
+        user = User.query.get(userId)
+        partner = User.query.get(partnerId)
 
-        )
-
+        new_conversation = Conversation()
+        new_conversation.users.append(user)
+        new_conversation.users.append(partner)
+        db.session.add(new_conversation)
+        db.session.commit()
+        return new_conversation.to_dict()
+        
 ########################## ENCRYPT/DECRYPT A MESSAGE #########################
 @message_routes.route('/encrypt', methods=['POST'])
 def encrypt_message():
