de8ec5f4d5a49a468c4d7715a23c3aa3388b926e
James Lentzsch <jrlentzsch@gmail.com>
2021-10-28 13:01:48 -0700

    store cleaned up

--- react-app/src/store/conversation.js
+++ react-app/src/store/conversation.js
@@ -3,31 +3,31 @@ const SEARCH_RESULTS = "conversation/SEARCH_RESULTS";
 const CLEAR_SEARCH = "conversation/CLEAR_SEARCH";
 const SEARCHED_USER = "conversation/SEARCHED_USER";
 const HANDLE_MESSAGES = "conversation/HANDLE_MESSAGES";
-const POST_NEW_CONVERSATION = "conversation/POST_NEW_CONVERSATION"
+const POST_NEW_CONVERSATION = "conversation/POST_NEW_CONVERSATION";
 
 export const currentConversation = (conversationId) => ({
     type: CURRENT_CONVERSATION,
     payload: conversationId
-})
+});
 
 export const clearSearch = () => ({
     type: CLEAR_SEARCH
-})
+});
 
 export const searchedUser = (userId) => ({
     type: SEARCHED_USER,
     payload: userId
-})
+});
 
 export const handleMessages = (message) => ({
     type: HANDLE_MESSAGES,
     payload: message
-})
+});
 
 const searchResults = (results) => ({
     type: SEARCH_RESULTS,
     payload: results
-})
+});
 
 const postNewConversation = (userId, partnerId) => ({
     type: POST_NEW_CONVERSATION,
@@ -35,31 +35,31 @@ const postNewConversation = (userId, partnerId) => ({
         userId,
         partnerId
     }
-})
+});
 
 
 /**************************** SEARCH CODENAMES ******************************/
 export const searchCodeNames = (codeName) => async (dispatch) => {
-    const response = await fetch(`/api/users/${codeName}`)
-    const data = await response.json()
+    const response = await fetch(`/api/users/${codeName}`);
+    const data = await response.json();
     if (data.errors) {
         return data;
-    }
+    };
 
-    dispatch(searchResults(data))
-}
+    dispatch(searchResults(data));
+};
 
 
 /**************************** GET ALL CONVERSATIONS WITH ************************/
 export const getAllConversationsWith = (userId) => async (dispatch) => {
-    const response = await fetch(`/api/messages/${userId}`)
-    const data = await response.json()
+    const response = await fetch(`/api/messages/${userId}`);
+    const data = await response.json();
     if (data.errors) {
         return data;
-    }
+    };
 
-    dispatch(searchResults(data))
-}
+    dispatch(searchResults(data));
+};
 
 /***************************** POST NEW CONVERSATION ***************************/
 export const createNewConvserstion = (userId, partnerId) => async (dispatch) => {
@@ -73,34 +73,34 @@ export const createNewConvserstion = (userId, partnerId) => async (dispatch) => 
             userId,
             partnerId
         })
-    })
+    });
     const data = await response.json();
     if (data.errors) {
-        return data
-    }
+        return data;
+    };
 
-    dispatch(postNewConversation(data))
+    dispatch(postNewConversation(data));
     return {};
-}
+};
 
 
-const initialState = { current: null, searchResults: {}, searchedUser: null,  }
+const initialState = { current: null, searchResults: {}, searchedUser: null,  };
 
 export default function conversationReducer(state=initialState, action) {
     switch (action.type) {
         case SEARCH_RESULTS:
-            return { ...state, searchResults: action.payload }
+            return { ...state, searchResults: action.payload };
         case CURRENT_CONVERSATION:
-            return { ...state, current: state.searchResults[action.payload] }
+            return { ...state, current: state.searchResults[action.payload] };
         case CLEAR_SEARCH:
-            return { ...state, searchResults: {}, searchedUser: null }
+            return { ...state, searchResults: {}, searchedUser: null };
         case SEARCHED_USER:
-            return { ...state, searchedUser: action.payload }
+            return { ...state, searchedUser: action.payload };
         case HANDLE_MESSAGES:
-            return { ...state, current: { ...state.current, messages: [...state.current.messages,  action.payload] } }
+            return { ...state, current: { ...state.current, messages: [...state.current.messages,  action.payload] } };
         case POST_NEW_CONVERSATION:
-            return { ...state, current: action.payload }
+            return { ...state, current: action.payload };
         default:
             return state;
-    }
-}
+    };
+};
--- react-app/src/store/enigma.js
+++ react-app/src/store/enigma.js
@@ -1,72 +1,72 @@
-const SET_PLUGBOARD = './enigma/SET_PLUGBOARD'
-const OUTPUT_MESSAGE = './enigma/OUTPUT_MESSAGE'
-const SET_ROTOR_TO_SET = './enigma/SET_ROTOR_TO_SET'
-const SET_ROTOR_1_NAME = './enigma/SET_ROTOR_1_NAME'
-const SET_ROTOR_1_POSITION = './enigma/SET_ROTOR_1_POSITION'
-const SET_ROTOR_2_NAME = './enigma/SET_ROTOR_2_NAME'
-const SET_ROTOR_2_POSITION = './enigma/SET_ROTOR_2_POSITION'
-const SET_ROTOR_3_NAME = './enigma/SET_ROTOR_3_NAME'
-const SET_ROTOR_3_POSITION = './enigma/SET_ROTOR_3_POSITION'
-const SET_REFLECTOR = './enigma/SET_REFLECTOR'
-const SET_CURRENT_CHAR = './enigma/SET_CURRENT_CHAR'
-const CLEAR_OUTPUT_MESSAGE = './enigma/CLEAR_OUTPUT_MESSAGE'
+const SET_PLUGBOARD = './enigma/SET_PLUGBOARD';
+const OUTPUT_MESSAGE = './enigma/OUTPUT_MESSAGE';
+const SET_ROTOR_TO_SET = './enigma/SET_ROTOR_TO_SET';
+const SET_ROTOR_1_NAME = './enigma/SET_ROTOR_1_NAME';
+const SET_ROTOR_1_POSITION = './enigma/SET_ROTOR_1_POSITION';
+const SET_ROTOR_2_NAME = './enigma/SET_ROTOR_2_NAME';
+const SET_ROTOR_2_POSITION = './enigma/SET_ROTOR_2_POSITION';
+const SET_ROTOR_3_NAME = './enigma/SET_ROTOR_3_NAME';
+const SET_ROTOR_3_POSITION = './enigma/SET_ROTOR_3_POSITION';
+const SET_REFLECTOR = './enigma/SET_REFLECTOR';
+const SET_CURRENT_CHAR = './enigma/SET_CURRENT_CHAR';
+const CLEAR_OUTPUT_MESSAGE = './enigma/CLEAR_OUTPUT_MESSAGE';
 
 export const setPlugboard = (char1, char2) => ({
     type: SET_PLUGBOARD,
     char1,
     char2
-})
+});
 
 export const setRotorToSet = (name) => ({
     type: SET_ROTOR_TO_SET,
     payload: name
-})
+});
 
 export const setRotor1name = (name) => ({
     type: SET_ROTOR_1_NAME,
     payload: name
-})
+});
 export const setRotor1position = (position) => ({
     type: SET_ROTOR_1_POSITION,
     payload: position
-})
+});
 
 export const setRotor2name = (name) => ({
     type: SET_ROTOR_2_NAME,
     payload: name
-})
+});
 export const setRotor2position = (position) => ({
     type: SET_ROTOR_2_POSITION,
     payload: position
-})
+});
 
 export const setRotor3name = (name) => ({
     type: SET_ROTOR_3_NAME,
     payload: name
-})
+});
 export const setRotor3position = (position) => ({
     type: SET_ROTOR_3_POSITION,
     payload: position
-})
+});
 
 export const setReflector = (setting) => ({
     type: SET_REFLECTOR,
     payload: setting
-})
+});
 
 export const setCurrentChar = (char) => ({
     type: SET_CURRENT_CHAR,
     payload: char
-})
+});
 
 export const clearOutputMessage = () =>({
     type: CLEAR_OUTPUT_MESSAGE,
-})
+});
 
 const outputMessage = (message) => ({
     type: OUTPUT_MESSAGE,
     payload: message
-})
+});
 
 
 
@@ -82,53 +82,53 @@ export const encryptMessage = (message, settings) => async (dispatch) => {
     });
 
     const { translatedMessage } = await response.json();
-    dispatch(outputMessage(translatedMessage))
-}
+    dispatch(outputMessage(translatedMessage));
+};
 
 /*********************** PLUGBOARD DEFAULT ********************/
 const plugboardAlpha = 'QWERTZUIOASDFGHJKPYXCVBNML'.split('')
-const plugboard = {}
+const plugboard = {};
 for (let char of plugboardAlpha) {
-    plugboard[char] = char
+    plugboard[char] = char;
 }
 
-const rotor1 = { name: 'III', position: 1 }
-const rotor2 = { name: 'II', position: 1 }
-const rotor3 = { name: 'I', position: 1 }
+const rotor1 = { name: 'III', position: 1 };
+const rotor2 = { name: 'II', position: 1 };
+const rotor3 = { name: 'I', position: 1 };
 
-const reflector = 'B'
+const reflector = 'B';
 
-const initialState = { plugboard, rotorToSet: '', rotor1, rotor2, rotor3, reflector, currentChar: '', outputMessage: ''}
+const initialState = { plugboard, rotorToSet: '', rotor1, rotor2, rotor3, reflector, currentChar: '', outputMessage: ''};
 
 export default function plugboardReducer(state=initialState, action) {
     switch (action.type) {
         case SET_PLUGBOARD:
-            state.plugboard[action.char1] = action.char2
-            state.plugboard[action.char2] = action.char1
-            return { ...state, plugboard: { ...state.plugboard } }
+            state.plugboard[action.char1] = action.char2;
+            state.plugboard[action.char2] = action.char1;
+            return { ...state, plugboard: { ...state.plugboard } };
         case SET_ROTOR_TO_SET:
-            return { ...state, rotorToSet: action.payload }
+            return { ...state, rotorToSet: action.payload };
         case SET_ROTOR_1_NAME:
-            return { ...state, rotor1: { ...state.rotor1, name: action.payload }}
+            return { ...state, rotor1: { ...state.rotor1, name: action.payload }};
         case SET_ROTOR_1_POSITION:
-            return { ...state, rotor1: { ...state.rotor1, position: action.payload }}
+            return { ...state, rotor1: { ...state.rotor1, position: action.payload }};
         case SET_ROTOR_2_NAME:
-            return { ...state, rotor2: { ...state.rotor2, name: action.payload }}
+            return { ...state, rotor2: { ...state.rotor2, name: action.payload }};
         case SET_ROTOR_2_POSITION:
-            return { ...state, rotor2: { ...state.rotor2, position: action.payload }}
+            return { ...state, rotor2: { ...state.rotor2, position: action.payload }};
         case SET_ROTOR_3_NAME:
-            return { ...state, rotor3: { ...state.rotor3, name: action.payload }}
+            return { ...state, rotor3: { ...state.rotor3, name: action.payload }};
         case SET_ROTOR_3_POSITION:
-            return { ...state, rotor3: { ...state.rotor3, position: action.payload }}
+            return { ...state, rotor3: { ...state.rotor3, position: action.payload }};
         case SET_REFLECTOR:
-            return { ...state, reflector: action.payload }
+            return { ...state, reflector: action.payload };
         case SET_CURRENT_CHAR:
-            return { ...state, currentChar: action.payload }
+            return { ...state, currentChar: action.payload };
         case OUTPUT_MESSAGE:
-            return { ...state, outputMessage: action.payload }
+            return { ...state, outputMessage: action.payload };
         case CLEAR_OUTPUT_MESSAGE:
-            return { ...state, outputMessage: null }
+            return { ...state, outputMessage: null };
         default:
             return state;
-    }
-}
+    };
+};
