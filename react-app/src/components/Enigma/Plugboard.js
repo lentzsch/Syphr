c1dab9e84c85cf5f96dd4059c6f37e32b402eda7
James Lentzsch <jrlentzsch@gmail.com>
2021-11-17 12:51:38 -0800

    plugboard bugs fixed and light indication added but server error now appearing on demo log in attempt

--- react-app/src/components/Enigma/Plugboard.js
+++ react-app/src/components/Enigma/Plugboard.js
@@ -16,25 +16,39 @@ const Plugboard = () => {
 
     const handleClick = (char) => (event) => {
         event.stopPropagation();
+        if (selectedCharacter && plugboardSettings[char] !== char) {
+            return;
+        }
 
         if (!selectedCharacter && plugboardSettings[char] !== char) {
             console.log('No selected char and plugboardsetting != char')
             console.log('Plugboard settings char: ', plugboardSettings[char], " char: ", char)
+            console.log("char to revert: ", charToRevert)
+            console.log("Selected char: ", selectedCharacter)
             setCharToRevert(plugboardSettings[char]);
             console.log(charToRevert)
             setSelectedCharacter(char);
-        } else if (charToRevert) {
+        } else if (selectedCharacter && charToRevert) {
             console.log("selected char and char to revert")
+            console.log('Plugboard settings char: ', plugboardSettings[char], " char: ", char)
             console.log("char to revert: ", charToRevert)
+            console.log("Selected char: ", selectedCharacter)
             dispatch(setPlugboard(selectedCharacter, char));
-            dispatch(setPlugboard(charToRevert, charToRevert))
+            dispatch(setPlugboard(charToRevert, charToRevert));
             setSelectedCharacter('');
+            setCharToRevert('')
         } else if (selectedCharacter) {
             console.log("seleceted char")
-            console.log(charToRevert)
+            console.log('Plugboard settings char: ', plugboardSettings[char], " char: ", char)
+            console.log("char to revert: ", charToRevert)
+            console.log("Selected char: ", selectedCharacter)
             dispatch(setPlugboard(selectedCharacter, char));
             setSelectedCharacter('');
         } else {
+            console.log("else")
+            console.log('Plugboard settings char: ', plugboardSettings[char], " char: ", char)
+            console.log("char to revert: ", charToRevert)
+            console.log("Selected char: ", selectedCharacter)
             setSelectedCharacter(char);
         }
     }
@@ -57,9 +71,19 @@ const Plugboard = () => {
                         <div className="plug-socket-lable" key={`${char}-socket-lable`}>
                             {char}
                         </div>
-                        <div className="plug-socket" id={`${char}-socket`} key={char} onClick={handleClick(plugboardSettings[char])} value={char}>
-                            {plugboardSettings[char]}
-                        </div>
+                        { plugboardSettings[char] === selectedCharacter
+                            ? <div className="plug-socket-selected" id={`${char}-socket`} key={char} onClick={handleClick(plugboardSettings[char])} value={char}>
+                                {plugboardSettings[char]}
+                            </div>
+                            :( plugboardSettings[char] !== char
+                            ? <div className="plug-socket-swapped" id={`${char}-socket`} key={char} onClick={handleClick(plugboardSettings[char])} value={char}>
+                                {plugboardSettings[char]}
+                            </div>
+                            :<div className="plug-socket" id={`${char}-socket`} key={char} onClick={handleClick(plugboardSettings[char])} value={char}>
+                                {plugboardSettings[char]}
+                            </div>
+                            )
+                        }
                     </div>
                 );
             })}
