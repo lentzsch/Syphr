4b6251369a6e61d4c242abe6df05158144faf81a
James Lentzsch <jrlentzsch@gmail.com>
2022-12-07 17:36:37 -0800

    production prefix function added to foreign keys

--- app/models/conversations_users.py
+++ app/models/conversations_users.py
@@ -2,8 +2,8 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 conversations_users = db.Table(
     "conversations_users",
-    db.Column('conversationId', db.Integer, db.ForeignKey('conversations.id'), primary_key = True),
-    db.Column('userId', db.Integer, db.ForeignKey('users.id'), primary_key = True)
+    db.Column('conversationId', db.Integer, db.ForeignKey(add_prefix_for_prod('conversations.id')), primary_key = True),
+    db.Column('userId', db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), primary_key = True)
 )
 
 if environment == "production":
--- app/models/message.py
+++ app/models/message.py
@@ -3,15 +3,15 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 class Message(db.Model):
     __tablename__ = 'messages'
-    
+
     if environment == "production":
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key = True)
     message = db.Column(db.Text, nullable = False)
     settings = db.Column(db.Text, nullable= False)
-    senderId = db.Column(db.Integer, db.ForeignKey('users.id'))
-    conversationId = db.Column(db.Integer, db.ForeignKey('conversations.id'))
+    senderId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')))
+    conversationId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('conversations.id')))
     createdAt = db.Column(db.DateTime, server_default = db.func.now())
     updatedAt = db.Column(db.DateTime, server_default=db.func.now())
     sender = db.relationship('User', backref="sent_messages")
